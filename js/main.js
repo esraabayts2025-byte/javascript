c94fefe59d8eeee91b97cdf2bd23ae3b66ae673e
esraa <esraabayts2025-byte@gmail.com>
2025-08-26 16:09:33 +0300

    add

---
+++ js/main.js
@@ -0,0 +1,6 @@
+<script>
+        alert("hello")
+        document.writeln("welcome")
+        console.log("in syria")
+        document.getElementById("container").innerHTML="js first class"
+    </script>
