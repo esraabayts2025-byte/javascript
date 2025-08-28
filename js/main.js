1f17a1358f64a94f282f1c2e3e4a9ed37b5c8774
yourName <email@gmail.com>
2025-08-28 16:26:48 +0300

    train

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
-<script>
-        alert("hello")
+{/* <script>
+      alert("hello")
         document.writeln("welcome")
         console.log("in syria")
-        document.getElementById("container").innerHTML="js first class"
-    </script>
+        document.getElementById("container").innerHTML="js first class" 
+    </script> */}
+    
+    
+    let x=1 ;y=5;z=7;e=3
+    let sum=x+y+z+e
+    let difference =x-y-z-e ;
+    let product = a * b;
+    let quotient = a / b;
+
+    console.log("Sum:", sum);
+    console.log("Difference:", difference);
+    console.log("Product:", product);
+    console.log("Quotient:", quotient);
+
+   
---
+++ js/x1.js
@@ -0,0 +1,11 @@
+let x = 70;
+ if(x>=60){
+        console.log("pass")
+    }
+    if(x<60){
+        console.log("failed")
+    }
+if(x==59){
+    x++;
+    console.log("pass with help")
+}
